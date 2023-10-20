03311912ca27bcfc026758154df46ab61719c7ac
Austinsmi <143511688+Austinsmi@users.noreply.github.com>
2023-10-20 09:35:18 -0400

    Add files via upload

--- code.py
+++ code.py
@@ -26,14 +26,13 @@ left_motor_backward = board.GP13
 pwm_La = pwmio.PWMOut(left_motor_forward, frequency=10000)
 pwm_Lb = pwmio.PWMOut(left_motor_backward, frequency=10000)
 
-Left_Motor = moto
-r.DCMotor(pwm_La, pwm_Lb)  # configurtion line is needed initilizes left motor
+Left_Motor = motor.DCMotor(pwm_La, pwm_Lb)  # configurtion line is needed initilizes left motor
 Left_Motor_speed = 0  # Initilizes left_motor_forward spped to 0
 
 Right_Motor = motor.DCMotor(pwm_Ra, pwm_Rb)
 Right_Motor_speed = 0
 
-while True:
+
 
     # Left_Motor_speed = -1  # This makes the left wheel move backwards
     # Left_Motor.throttle
@@ -50,18 +49,38 @@ def backwards():
     Right_Motor_speed = -1  # makes  the left whell move fowards
     Right_Motor.throttle = Right_Motor_speed
 def turn_left():
-    Left_Motor_speed = -1  # makes  the left whell move fowards
+    Left_Motor_speed = 1  # makes  the left whell move fowards
     Left_Motor.throttle = Left_Motor_speed
-    Right_Motor_speed = 1 # makes  the left whell move fowards
+    Right_Motor_speed = -1 # makes  the left whell move fowards
     Right_Motor.throttle = Right_Motor_speed
 def turn_right():
     Right_Motor_speed = -1 # makes  the left whell move fowards
     Right_Motor.throttle = Right_Motor_speed
+    Left_Motor_speed = 1  # makes  the left whell move fowards
+    Left_Motor.throttle = Left_Motor_speed
+
 def Stop():
     Right_Motor_speed = 0 # makes  the left whell move fowards
     Right_Motor.throttle = Right_Motor_speed
     Left_Motor_speed = 0  # makes  the left whell move fowards
     Left_Motor.throttle = Left_Motor_speed
+
+while True:
+    forwards()
+    time.sleep(2)
+    turn_left()
+    time.sleep(1)
+    forwards()
+    time.sleep(2)
+    turn_left()
+    time.sleep(1)
+    forwards()
+    time.sleep(1)
+    turn_right()
+
+
+
+
 
 
     #time.sleep(3)
@@ -70,36 +89,4 @@ def Stop():
     #time.sleep(2)
     #Left_Motor_speed = -1  # makes  the left whell move fowards
 
-    #Left_Motor.throttle = Left_Motor_speed
-    #Right_Motor_speed = 1  # makes  the left whell move fowards
-    #Right_Motor.throttle = Right_Motor_speed
-    #time.sleep(1)
-    #Left_Motor_speed = -1  # makes  the left whell move fowards
-    #Left_Motor.throttle = Left_Motor_speed
-    #time.sleep()
-
-#
-# Left_Motor_speed = -1  # makes  the left whell move fowards
-# Left_Motor.throttle = Left_Motor_speed
-# Right_Motor_speed = 1  # makes  the left whell move fowards
-# Right_Motor.throttle =Right_Motor_speed
-
-# Left_Motor_speed = 1  # makes  the left whell move fowards
-# Left_Motor.throttle = Left_Motor_speed
-# Right_Motor_speed = -1  # makes  the left whell move fowards
-# Right_Motor.throttle = Right
-
-
-# time.sleep(3)
-
-# Right_Motor_speed = -1
-# Right_Motor.throttle = Right_Motor_speed
-# time.sleep(3)
-# Left_Motor_speed = 1
-# Left_Motor.throttle = Left_Motor_speed
-#  time.sleep(1)
- while true:
-    forwards()
-
-# Write your code here :-)
-
+    #Left_Motor.throttle = Left_Motor_speed# Write your code here :-)
